468164a2816616329be56a610cab8c0f6720c5b4
sena <sena@sena-MS-7817>
2017-05-03 08:53:05 -0500

    V.1

--- src/app/Empresa.java
+++ src/app/Empresa.java
@@ -10,5 +10,54 @@ package app;
  * @author sena
  */
 public class Empresa {
+      private String nit;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    public void setNit (String nitPara)
+    {
+    this.nit=nitPara;
+    }
+    public String getNit() 
+    {
+     return this.nit;
+    }
     
+    public void setNombre (String nombrePara) 
+    {
+    this.nombre=nombrePara;
+    }
+    public String getNombre()
+    {
+    return this.nombre;
+    }
+    
+    public void setDireccion (String direccionPara) 
+    {
+    this.direccion=direccionPara;
+    }
+    public String getDireccion()
+    {
+    return this.direccion;
+    }
+    
+    public void setTelefono (String telefonoPara) 
+    {
+    this.telefono=telefonoPara;
+    }
+    public String getTelefono()
+    {
+    return this.telefono;
+    }
+    
+     public void vender()       
+    {
+    System.out.print(nit+"Esta vendiendo");
+    }
+     
+     public void brindar()       
+    {
+    System.out.print(nombre+"Esta brindado servicios");
+    }
+     
 }
